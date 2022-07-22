1c2e9d4ebf157f7872088014ddc9aa88ee0fbe84
David Jones <sc16dj@leeds.ac.uk>
2022-07-22 21:11:06 +0100

    Initial Commit

---
+++ FrFinder/FrFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrFinderApp.swift
+//  FrFinder
+//
+//  Created by David Jones on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct FrFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
